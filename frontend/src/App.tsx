8fea9554d0eb5ed0ff9c5333ddbc605bae506d3c
tokoroten-lab <tokoroten.lab@gmail.com>
2020-09-29 20:35:23 +0900

    Add firebase auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,11 +4,36 @@ import {
   Switch,
   Route,
 } from "react-router-dom";
+import useUser from './hooks/useUser';
 import EngineerUserAbilityPage from './pages/EngineerUserAbilityPage';
 
 function App() {
+  const {user, create, login, logout} = useUser();
+
   return (
     <div className="App">
+      { user !== null &&
+        <div>
+          <p>{user.email}</p>
+          <button
+            onClick={() => logout()}
+          >
+            ログアウト
+          </button>
+        </div>
+      }
+      <button
+        onClick={async () => {
+          await create("yeah@example.com", "password");
+        }}
+      >
+        作成
+      </button>
+      <button
+        onClick={async () => await login("yeah@example.com", "password")}
+      >
+        ログイン
+      </button>
       <Router>
         <Switch>
           <Route path="/user/engineer/:engineerUserId">
