e2014648ed634865fb04f3bbb5185663a9fe757e
tokoroten-lab <tokoroten.lab@gmail.com>
2020-09-29 21:22:18 +0900

    Add drawer on frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,45 +1,126 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
 import useUser from './hooks/useUser';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText
+} from '@material-ui/core';
+import {
+  Inbox as InboxIcon,
+  Mail as MailIcon,
+} from '@material-ui/icons';
+import ListItemLink from './components/ListItemLink';
 import EngineerUserAbilityPage from './pages/EngineerUserAbilityPage';
 
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  content: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+  }
+}));
+
 function App() {
-  const {user, create, login, logout} = useUser();
+  const classes = useStyles();
+  const {user, logout} = useUser();
+
+  useEffect(() => {
+    console.log(user)
+  }, [user])
 
   return (
     <div className="App">
-      { user !== null &&
-        <div>
-          <p>{user.email}</p>
-          <button
-            onClick={() => logout()}
-          >
-            ログアウト
-          </button>
-        </div>
-      }
-      <button
-        onClick={async () => {
-          await create("yeah@example.com", "password");
-        }}
-      >
-        作成
-      </button>
-      <button
-        onClick={async () => await login("yeah@example.com", "password")}
-      >
-        ログイン
-      </button>
       <Router>
-        <Switch>
-          <Route path="/user/engineer/:engineerUserId">
-            <EngineerUserAbilityPage />
-          </Route>
-        </Switch>
+        <Drawer
+          className={classes.drawer}
+          variant="permanent"
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+          anchor="left"
+        >
+          <div className={classes.toolbar} />
+          <List>
+            { !user && (
+              <>
+                <ListItemLink
+                  to="/register"
+                  primary="新規登録"
+                />
+                <ListItemLink
+                  to="/login"
+                  primary="ログイン"
+                />
+              </>
+            )}
+            { user && (
+              <ListItem
+                button
+                key={"logout"}
+                onClick={async () => {
+                  await logout()
+                }}
+              >
+                <ListItemText primary={"ログアウト"} />
+              </ListItem>
+            )}
+          </List>
+          <Divider />
+          <List>
+            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+              <ListItem button key={text} onClick={() => console.log("yeah")}>
+                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+        </Drawer>
+        { user !== null &&
+          <div>
+            <p>{user.email}</p>
+            <button
+              onClick={() => logout()}
+            >
+              ログアウト
+            </button>
+          </div>
+        }
+        <div className={classes.content}>
+          <Switch>
+            <Route path="/user/engineer/:engineerUserId">
+              <EngineerUserAbilityPage />
+            </Route>
+            <Route path="/register">
+              <p>Register</p>
+            </Route>
+            <Route path="/login">
+              <p>Login</p>
+            </Route>
+          </Switch>
+        </div>
       </Router>
     </div>
   );
---
+++ frontend/src/components/ListItemLink.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link as RouterLink, LinkProps as RouterLinkProps } from 'react-router-dom';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
+interface ListItemLinkProps {
+  icon?: React.ReactElement;
+  primary: string;
+  to: string;
+}
+
+export default function ListItemLink(props: ListItemLinkProps) {
+  const { icon, primary, to } = props;
+
+  const renderLink = React.useMemo(
+    () =>
+      React.forwardRef<any, Omit<RouterLinkProps, 'to'>>((itemProps, ref) => (
+        <RouterLink to={to} ref={ref} {...itemProps} />
+      )),
+    [to],
+  );
+
+  return (
+    <li>
+      <ListItem button component={renderLink}>
+        {icon ? <ListItemIcon>{icon}</ListItemIcon> : null}
+        <ListItemText primary={primary} />
+      </ListItem>
+    </li>
+  );
+}
+
