54a2f979c73efbdd53b498a1d1a40b0235aa6d32
tokoroten-lab <tokoroten.lab@gmail.com>
2020-09-29 13:40:50 +0900

    Add EngineerUserAbilityReport controller

---
+++ backend/controller/engineer_user_ability_report.go
@@ -0,0 +1,54 @@
+package controller
+
+import (
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/labstack/echo/v4"
+	"github.com/tokoroten-lab/engineer-ability-visualizer/engineer_user"
+	"github.com/tokoroten-lab/engineer-ability-visualizer/repository"
+)
+
+type EngineerUserAbilityReport struct {
+	db *sqlx.DB
+}
+
+func NewEngineerUserAbilityReport(db *sqlx.DB) *EngineerUserAbilityReport {
+	return &EngineerUserAbilityReport{db: db}
+}
+
+func (a *EngineerUserAbilityReport) Create(c echo.Context) error {
+	idParam := c.Param("id")
+	id, err := strconv.ParseUint(idParam, 10, 64)
+	if err != nil {
+		log.Println(err)
+		return c.String(http.StatusBadRequest, "Engineer user id is invalid")
+	}
+
+	mockData, err := engineer_user.CalcEngineerUserAbility(id)
+	if err != nil {
+		log.Println(err)
+		return c.String(http.StatusInternalServerError, "Calc engineer ability has failed")
+	}
+
+	return c.JSON(http.StatusOK, mockData)
+}
+
+func (a *EngineerUserAbilityReport) Get(c echo.Context) error {
+	idParam := c.Param("id")
+	id, err := strconv.ParseUint(idParam, 10, 64)
+	if err != nil {
+		log.Println(err)
+		return c.String(http.StatusBadRequest, "Engineer user id is invalid")
+	}
+
+	mockData, err := repository.GetEngineerUserAbilityReports(id)
+	if err != nil {
+		log.Println(err)
+		return c.String(http.StatusInternalServerError, "Getting engineer ability has failed")
+	}
+
+	return c.JSON(http.StatusOK, mockData)
+}
--- backend/main.go
+++ backend/main.go
@@ -2,18 +2,14 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"os"
-	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/tokoroten-lab/engineer-ability-visualizer/controller"
-	"github.com/tokoroten-lab/engineer-ability-visualizer/engineer_user"
-	"github.com/tokoroten-lab/engineer-ability-visualizer/repository"
 )
 
 func main() {
@@ -25,6 +21,10 @@ func main() {
 		panic(err)
 	}
 
+	// Controllers
+	engineerUserController := controller.NewEngineerUser(db)
+	engineerUserAbilityReportController := controller.NewEngineerUserAbilityReport(db)
+
 	// Echo instance
 	e := echo.New()
 
@@ -33,15 +33,14 @@ func main() {
 	e.Use(middleware.Recover())
 	e.Use(middleware.CORS())
 
-	engineerUserController := controller.NewEngineerUser(db)
-
 	// Routes
 	e.GET("/", hello)
 
 	e.GET("/user/engineer", engineerUserController.GetAll)
 	e.GET("/user/engineer/:id", engineerUserController.Get)
-	e.GET("/user/engineer/:id/ability", getEngineerUserAbility)
-	e.POST("/user/engineer/:id/ability", postEngineerUserAbility)
+
+	e.POST("/user/engineer/:id/ability", engineerUserAbilityReportController.Create)
+	e.GET("/user/engineer/:id/ability", engineerUserAbilityReportController.Get)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
@@ -50,38 +49,4 @@ func main() {
 // Handler
 func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
-}
-
-func getEngineerUserAbility(c echo.Context) error {
-	idParam := c.Param("id")
-	id, err := strconv.ParseUint(idParam, 10, 64)
-	if err != nil {
-		log.Println(err)
-		return c.String(http.StatusBadRequest, "Engineer user id is invalid")
-	}
-
-	mockData, err := repository.GetEngineerUserAbilityReports(id)
-	if err != nil {
-		log.Println(err)
-		return c.String(http.StatusInternalServerError, "Getting engineer ability has failed")
-	}
-
-	return c.JSON(http.StatusOK, mockData)
-}
-
-func postEngineerUserAbility(c echo.Context) error {
-	idParam := c.Param("id")
-	id, err := strconv.ParseUint(idParam, 10, 64)
-	if err != nil {
-		log.Println(err)
-		return c.String(http.StatusBadRequest, "Engineer user id is invalid")
-	}
-
-	mockData, err := engineer_user.CalcEngineerUserAbility(id)
-	if err != nil {
-		log.Println(err)
-		return c.String(http.StatusInternalServerError, "Calc engineer ability has failed")
-	}
-
-	return c.JSON(http.StatusOK, mockData)
 }
