35771dfb6487581255271277ca6fb426d4f46c0b
tokoroten-lab <tokoroten.lab@gmail.com>
2020-09-29 11:49:46 +0900

    Display engineer ability

--- frontend/src/components/EngineerUserAbility.tsx
+++ frontend/src/components/EngineerUserAbility.tsx
@@ -6,20 +6,42 @@ interface Props {
 }
 
 export default function EngineerUserAbility(props: Props) {
+  function calcDetectabilityScore(ability: EngineerUserAbilityType) {
+    return ability.issueScore;
+  }
+
+  function calcSolvingScore(ability: EngineerUserAbilityType) {
+    return ability.projectScore
+      + ability.repositoryScore
+      + ability.commitScore
+      + ability.pullreqScore;
+  }
+
+  function calcSpeedScore(ability: EngineerUserAbilityType) {
+    return ability.speedScore;
+  }
+
   return (
     <div>
     { props.abilities.map((ability) => {
       return (
         <div key={ability.id}>
-          <p>{ability.id}</p>
-          <p>{ability.engineerUserId}</p>
-          <p>{ability.projectScore}</p>
-          <p>{ability.repositoryScore}</p>
-          <p>{ability.commitScore}</p>
-          <p>{ability.pullreqScore}</p>
-          <p>{ability.issueScore}</p>
-          <p>{ability.speedScore}</p>
-          <p>{ability.createdAt}</p>
+          <h2>能力</h2>
+          <div>
+            <h3>発見力 {calcDetectabilityScore(ability)}</h3>
+            <p>{ability.issueScore}</p>
+          </div>
+          <div>
+            <h3>解決力 {calcSolvingScore(ability)}</h3>
+            <p>プロジェクトスコア {ability.projectScore}</p>
+            <p>リポジトリスコア {ability.repositoryScore}</p>
+            <p>コミットスコア {ability.commitScore}</p>
+            <p>プルリクエストスコア {ability.pullreqScore}</p>
+          </div>
+          <div>
+            <h3>スピード {calcSpeedScore(ability)}</h3>
+            <p>{ability.speedScore}</p>
+          </div>
         </div>
       );
     })}
