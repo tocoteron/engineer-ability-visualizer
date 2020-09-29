c8f83544593f5b2fdc231838538e0ea3a14ab163
tokoroten-lab <tokoroten.lab@gmail.com>
2020-09-29 12:56:53 +0900

    Add json info

--- backend/model/engineer_user.go
+++ backend/model/engineer_user.go
@@ -1,10 +1,10 @@
 package model
 
 type EngineerUser struct {
-	ID          uint64
-	FirebaseUID string
-	GitHubToken string
-	DisplayName string
-	Email       string
-	PhotoURL    string
+	ID          uint64 `json:"id"`
+	FirebaseUID string `json:"firebaseUID"`
+	GitHubToken string `json:"githubToken"`
+	DisplayName string `json:"displayName"`
+	Email       string `json:"email"`
+	PhotoURL    string `json:"photoURL"`
 }
