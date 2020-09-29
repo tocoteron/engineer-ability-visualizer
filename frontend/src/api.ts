01ddfcf8eedacca83ad0aa0710036abf291b0422
tokoroten-lab <tokoroten.lab@gmail.com>
2020-09-29 13:05:06 +0900

    Add getEngineerUser api

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,14 +1,23 @@
-import EngineerUserAbilityReportType from "./models/EngineerUserAbilityReport";
+import EngineerUserAbilityReport from "./models/EngineerUserAbilityReport";
+import EngineerUser from "./models/EngineerUser";
 
 const API_BASE_URL = "http://localhost:1323";
 
-async function getEngineerUserAbilityReports(engineerUserId: number): Promise<EngineerUserAbilityReportType[]> {
+async function getEngineerUserAbilityReports(engineerUserId: number) {
     const res = await fetch(`${API_BASE_URL}/user/engineer/${engineerUserId}/ability`);
-    const json = await res.json();
+    const json: EngineerUserAbilityReport[] = await res.json();
+
+    return json;
+}
+
+async function getEngineerUser(engineerUserId: number) {
+    const res = await fetch(`${API_BASE_URL}/user/engineer/${engineerUserId}`);
+    const json: EngineerUser  = await res.json();
 
     return json;
 }
 
 export default {
     getEngineerUserAbilityReports,
+    getEngineerUser,
 }
