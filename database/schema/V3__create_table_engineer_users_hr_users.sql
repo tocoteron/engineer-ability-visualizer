d0b4510a84b88264abb3f15f3c5171bf98dbad54
tokoroten-lab <tokoroten.lab@gmail.com>
2020-09-30 19:27:13 +0900

    Update db schema

--- database/schema/V3__create_table_engineer_users_hr_users.sql
+++ database/schema/V3__create_table_engineer_users_hr_users.sql
@@ -2,6 +2,7 @@ CREATE TABLE `engineer_users_hr_users` (
   `id` int unsigned PRIMARY KEY AUTO_INCREMENT,
   `engineer_users_id` int unsigned,
   `hr_users_id` int unsigned,
+  UNIQUE (`engineer_users_id`, `hr_users_id`),
   FOREIGN KEY (`engineer_users_id`) REFERENCES `engineer_users` (`id`) ON UPDATE CASCADE,
   FOREIGN KEY (`hr_users_id`) REFERENCES `hr_users` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
