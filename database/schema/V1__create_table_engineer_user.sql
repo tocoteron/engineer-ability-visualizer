3a44adfed69389f72c3c29b61fbe2eb597ef5806
tokoroten-lab <tokoroten.lab@gmail.com>
2020-09-30 09:53:33 +0900

    Change db schema

--- database/schema/V1__create_table_engineer_user.sql
+++ database/schema/V1__create_table_engineer_user.sql
@@ -1,9 +1,6 @@
 CREATE TABLE `engineer_users` (
   `id` int unsigned PRIMARY KEY AUTO_INCREMENT,
-  `firebase_uid` varchar(255) UNIQUE NOT NULL,
-  `github_token` varchar(255) UNIQUE NOT NULL,
-  `email` varchar(255),
-  `login_name` varchar(255) NOT NULL,
+  `login_name` varchar(255) UNIQUE NOT NULL,
   `display_name` varchar(255) NOT NULL,
   `photo_url` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
